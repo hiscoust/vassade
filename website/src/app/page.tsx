18fa3b09ad0b1ee59d7fd2b525f08bd69d9a64d0
bilal <bilalhennouni@gmail.com>
2023-10-30 13:40:41 +0100

    backup

---
+++ website/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar/Navbar"
+
+function Home() {
+  return (
+    <div className="">
+      <Navbar />
+      Hallo Zuahir
+    </div>
+  )
+}
+
+export default Home
---
+++ website/src/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            Lgoo
+        </div>
+        <div className='flex items-center'>
+            <a><div>Startseite</div></a>
+            <a><div>Über uns</div></a>
+            <a><div>Leistungen</div></a>
+            <a><div>Energieberatung</div></a>
+            <a><div>Förderung</div></a>
+            <a><div>Kontakt</div></a>
+            <a><div>Link 1</div></a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
